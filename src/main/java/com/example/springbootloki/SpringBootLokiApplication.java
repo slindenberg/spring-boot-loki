ce1a595a2bcbfcd190245ec3e634b4ddcca26686
070636 <stefan.lindenberg@hdi.de>
2024-04-08 09:56:45 +0200

    - docker compose, database

--- src/main/java/com/example/springbootloki/SpringBootLokiApplication.java
+++ src/main/java/com/example/springbootloki/SpringBootLokiApplication.java
@@ -8,11 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @Slf4j
 @RequiredArgsConstructor
-//@EnableScheduling
+@EnableScheduling
 public class SpringBootLokiApplication implements ApplicationListener<ApplicationReadyEvent> {
 
 	private final SomeEntityRepository database;
